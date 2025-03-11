90731a44513f3b30e678b8c9433edced61de6dda
Noor Hosain Arif <165730242+noor-arif@users.noreply.github.com>
2025-03-11 20:13:38 +0300

    Add files via upload

---
+++ Week 19/Day 1/B. Segment Tree for the Minimum.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+// #define int long long int
+using namespace std;
+
+const int maxN = 1e5 + 9;
+int a[maxN], t[4 * maxN];
+
+void build(int n, int b, int e)
+{
+    if (b == e)
+    {
+        t[n] = a[b];
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    t[n] = min(t[l], t[r]);
+}
+
+void update(int n, int b, int e, int i, int v)
+{
+    if (i < b or i > e)
+        return;
+    if (b == e)
+    {
+        t[n] = v;
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    update(l, b, mid, i, v);
+    update(r, mid + 1, e, i, v);
+    t[n] = min(t[l], t[r]);
+}
+
+int query(int n, int b, int e, int i, int j)
+{
+    if (e < i or b > j)
+        return INT_MAX;
+    if (b >= i and e <= j)
+        return t[n];
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    return min(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    build(1, 1, n);
+
+    while (m--)
+    {
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            int i, v;
+            cin >> i >> v;
+            i++;
+            update(1, 1, n, i, v);
+        }
+        else
+        {
+            int l, r;
+            cin >> l >> r;
+            l++;
+            cout << query(1, 1, n, l, r) << endl;
+        }
+    }
+    return 0;
+}
